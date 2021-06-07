622742c9a1de7f781974ee8e85d295d979ec370a
Mahmoud-Rubaigit config --global user.name Mahmoud-Rubaigit config --global user.name Mahmoud-Rubai <mahmoudrab1999@gmail.com>
2021-06-08 00:11:09 +0300

    first commit

--- app/Http/Controllers/contactcontroller.php
+++ app/Http/Controllers/contactcontroller.php
@@ -1,30 +1,81 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\contact;
+
+
+use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class contactcontroller extends Controller
+class ContactController extends Controller
 {
-
-    public function index(){
-        $contacts = contact::all();
-
-        return view('contact-list',compact('contacts'));
-
+    public function __construct(){
+        $this->middleware('auth');
     }
+    public function index(){
+        $contacts= Contact::where('user_id', Auth::user()->id)->get();
+
+         return view('index',compact('contacts'));
+    }
+
     public function contact(){
         return view('contact');
     }
 
     public function store(Request $request){
-    $contact = new contact;
 
-    $contact->fName =$request->firstName;
-    $contact->lName =$request->lastName;
-    $contact->email =$request->email;
+        $data = $request->validate([
+            'firstName' => 'required|min:5|max:10' ,
+            'lastName' => 'required' ,
+            'email' => 'required'
+        ]);
 
-    $contact->save();
-return redirect()->back();
+
+        $contact = new Contact;
+
+        $contact->fName = $request->firstName;
+        $contact->lName = $request->lastName;
+        $contact->email = $request->email;
+        $contact->user_id = Auth::user()->id;
+
+        $contact->save();
+        return redirect('/');
+
     }
+
+    public function destroy($id){
+
+        $contact = Contact::find($id);
+        $contact->delete();
+        return redirect()-> back();
+
+    }
+
+    public function edit($id){
+       $contact =Contact::findOrFail($id);
+
+       return view('edit',compact('contact'));
+    }
+
+
+    public function update(Request $request , $id){
+        $contact = new Contact;
+
+        $contact->fName = $request->firstName;
+        $contact->lName = $request->lastName;
+        $contact->email = $request->email;
+        $contact->save();
+        $contact = Contact::find($id);
+        $contact->delete();
+        return redirect('/');
+
+
+
+    }
+
+
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,13 @@ Route::get('/',[contactcontroller::class,'index'] );
 Route::get('contact', [contactcontroller::class, 'contact']);
 Route::post('store', [contactcontroller::class, 'store']);
 
+Route::get('contcat.create', [ContcatController::class, 'create'])->name('contcat.create');
+Route::get('contcat.edit/{id}', [ContcatController::class, 'edit'])->name('contcat.edit');
+Route::put('contcat.update/{id}', [ContcatController::class, 'update'])->name('contcat.update');
+Route::delete('contcat.destroy/{id}', [ContcatController::class, 'destroy'])->name('contcat.destroy');
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
